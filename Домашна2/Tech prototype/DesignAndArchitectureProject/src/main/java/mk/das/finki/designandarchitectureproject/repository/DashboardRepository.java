9b321048f35d13da83e39190b4c5750f759491f0
IamMistake <59539303+IamMistake@users.noreply.github.com>
2024-12-03 00:48:58 +0100

    adding tech prototype

---
+++ Домашна2/Tech prototype/DesignAndArchitectureProject/src/main/java/mk/das/finki/designandarchitectureproject/repository/DashboardRepository.java
@@ -0,0 +1,7 @@
+package mk.das.finki.designandarchitectureproject.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DashboardRepository {
+}
---
+++ Домашна2/Tech prototype/DesignAndArchitectureProject/src/main/java/mk/das/finki/designandarchitectureproject/service/implementation/CompanyServiceImpl.java
@@ -0,0 +1,7 @@
+package mk.das.finki.designandarchitectureproject.service.implementation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompanyServiceImpl {
+}
